f3a54c59e2911914efbe966e83398921163f3c70
jtschams <jtschams@yahoo.com>
2024-05-09 18:24:43 -0500

    added TODOs. added sequelize sync to server. added tag routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,17 +5,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
+  // TODO: find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+  // TODO: find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
-// create new product
+// TODO: create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -47,7 +47,7 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// TODO: update product
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -93,7 +93,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  // TODO: delete one product by its `id` value
 });
 
 module.exports = router;
